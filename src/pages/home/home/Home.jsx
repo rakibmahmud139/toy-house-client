c035d167510ae4eece44ab1cb3ae42df4e63990d
Rakibmahmud <hasansaikat74@gmail.com>
2023-07-28 18:35:53 +0600

    added gallery section

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,32 +1,21 @@
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
+import GallerySection from '../../../share/gallerySection/GallerySection';
 
 const Home = () => {
     return (
-        <div className='relative mb-24'>
-            <img className='w-full h-[660px] rounded-xl ' src="https://i.ibb.co/TW9XW3y/pexels-eva-bronzini-5501994.jpg" alt="" />
-            <div className='absolute rounded-xl h-full  left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] space-y-6 '>
+        <div>
+            <div className='relative mb-16'>
+                <img className='w-full h-[660px] rounded-xl ' src="https://i.ibb.co/TW9XW3y/pexels-eva-bronzini-5501994.jpg" alt="" />
+                <div className='absolute rounded-xl h-full  left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] space-y-6 '>
 
-                <div className='mt-48 ml-24 mr-[782px] text-3xl'>
-                    <TypeAnimation
-                        sequence={[
-                            'Development\n',
-                            1000,
-                            'Development and sale\n',
-                            1000,
-                            "Development and sale  of children's\n",
-                            1000,
-                            "Development and sale  of children's car toys",
-                            1000,
-                        ]}
-                        speed={25}
-                        style={{ fontSize: '2em', whiteSpace: 'pre-line', }}
-                        repeat={Infinity}
-                    />
+                    <h3 className='mt-48 ml-24 mr-[782px] text-5xl font-bold'>Development and sale  of children's car toys</h3>
+
+                    <p className='ml-24'>Welcome to Car Toys, your ultimate destination <br /> for all things related to toy cars! Our website is a virtual wonderland for car enthusiasts, <br /> kids, and collectors alike.  Step into a world where imagination meets automotive fascination, and let the adventure begin!</p>
+                    <button className='btn btn-outline btn-accent ml-24'>All Toys</button>
                 </div>
-                <p className='ml-24'>Welcome to Car Toys, your ultimate destination <br /> for all things related to toy cars! Our website is a virtual wonderland for car enthusiasts, <br /> kids, and collectors alike.  Step into a world where imagination meets automotive fascination, and let the adventure begin!</p>
-                <button className='btn btn-outline btn-accent ml-24'>All Toys</button>
             </div>
+            <GallerySection />
         </div>
     );
 };
--- src/share/gallerySection/GallerySection.jsx
+++ src/share/gallerySection/GallerySection.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 const GallerySection = () => {
     return (
-        <div>
+        <div className='mb-16 text-center text-2xl text-accent'>
+            <TypeAnimation
+                sequence={[
+                    'There are some of car toys\n',
+                    1000,
+                    'There are some of car toys Bus,\n',
+                    1000,
+                    "There are some of car toys Train,\n",
+                    1000,
+                    "There are some of car toys Motorcycle etc.",
+                    1000,
+                ]}
+                speed={25}
+                style={{ fontSize: '2em', whiteSpace: 'pre-line', }}
+                repeat={Infinity}
+            />
 
+            <div className='grid grid-cols-1 lg:grid-cols-3'>
+                <div className="card w-96 bg-base-100 shadow-xl mt-12">
+                    <figure><img className='p-4 w-80 h-80 rounded-3xl' src="https://images.unsplash.com/photo-1581235720704-06d3acfcb36f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <p className='text-3xl'>Mini-Jeep</p>
+                        <p>This is mini jeep car. This toys are very strong to play</p>
+                    </div>
+                </div>
+
+                <div className="card w-96 bg-base-100 shadow-xl mt-12">
+                    <figure><img className='p-4 w-80 h-80 rounded-3xl' src="https://images.unsplash.com/photo-1453053507108-9f5456eb481f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dG95cyUyMHRyYWlufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <p className='text-3xl'>Mini-Train</p>
+                        <p>This is mini jeep car. This toys are very strong to play</p>
+                    </div>
+                </div>
+
+                <div className="card w-96 bg-base-100 shadow-xl mt-12">
+                    <figure><img className='p-4 w-80 h-80 rounded-3xl' src="https://images.unsplash.com/photo-1517649187581-ad0f359f0092?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHRveXMlMjB0cmFpbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <p className='text-3xl'>Mini-Track</p>
+                        <p>This is mini jeep car. This toys are very strong to play</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
